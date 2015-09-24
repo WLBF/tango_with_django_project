6f7a8b2e6119bd33a1b68de72d871bc8e28b3714
WLBF <lbf1353@live.com>
2015-09-24 12:06:59 +0800

    18

--- rango/urls.py
+++ rango/urls.py
@@ -14,4 +14,6 @@ urlpatterns = patterns('',
     url(r'^goto/$', views.track_url, name='goto'),
     url(r'^edit_profile/$', views.edit_profile, name='edit_profile'),
     url(r'^profile/$', views.profile, name='profile'),
+    url(r'^like_category/$', views.like_category, name='like_category'),
+    url(r'^suggest_category/$', views.suggest_category, name='suggest_category'),
 )
--- rango/views.py
+++ rango/views.py
@@ -12,6 +12,19 @@ from django.shortcuts import redirect
 from django.contrib.auth.models import User
 
 
+
+def get_category_list(max_results=0, starts_with=''):
+    cat_list = []
+    if starts_with:
+        cat_list = Category.objects.filter(name__istartwith=starts_with)
+
+    if max_results > 0:
+        if len(cat_list) > max_results:
+            cat_list = cat_list[:max_results]
+
+    return cat_list
+
+
 def index(request):
 
     category_list = Category.objects.order_by('-likes')[:5]
@@ -284,7 +297,7 @@ def track_url(request):
 
 
 
-
+@login_required
 def edit_profile(request):
    
     url = '/rango/'
@@ -331,3 +344,32 @@ def profile(request):
     context_dict['userprofile'] = up
     return render(request, 'rango/profile.html', context_dict)
 
+
+@login_required
+def like_category(request):
+
+    cat_id = None
+    if request.method == 'GET':
+        cat_id = request.GET['category_id']
+
+    likes = 0
+    if cat_id:
+        cat = Category.objects.get(id=int(cat_id))
+        if cat:
+            likes = cat.likes+1
+            cat.likes = likes
+            cat.save()
+
+    return HttpResponse(likes)
+
+
+def suggest_category(request):
+
+    cat_list = []
+    starts_with = ''
+    if request.method == 'GET':
+        starts_with = request.GET['suggestion']
+
+    cat_list = get_category_list(8, starts_with)
+
+    return render(request, 'rango/category_list.html', {'cat_list': cat_list})
